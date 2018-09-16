39231c3043970a69d7796bf98421754d4767a04d
ADMINRG-KO0BU21\Techlets <asad.raza@techletspk.com>
2018-09-17 01:10:31 +0500

    ''

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  public message =""
+
+
+  click(){
+    this.message = "Thanks for wasting your time";
+
+    setTimeout(()=>{
+      this.message="";
+    },2000);
+  }
 }
+
+
+
