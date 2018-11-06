77e39165154b628282980b9fa8dc8a6e4f554a23
ADMINRG-KO0BU21\Techlets <asad.raza@techletspk.com>
2018-11-06 23:05:12 +0500

    ''

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +7,14 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   public message =""
+  public title = "It Does Nothing";
+  public randomArray = ["它什么都没做","No hace nada","यह कुछ भी नहीं करता है","لا يفعل شيئا","Não faz nada","এটা কিছুই না","Он ничего не делает",
+  "それは何もしない","Iku ora","Es tut nichts","아무것도하지 않는다","هیچ کاری نمی کند","Ça ne fait rien","Hiçbir şey yapmıyor","It Does Nothing"]
 
 
   click(){
     this.message = "Thanks for wasting your time";
-
+    this.title = this.randomArray[Math.floor(Math.random()*10)];
     setTimeout(()=>{
       this.message="";
     },2000);
